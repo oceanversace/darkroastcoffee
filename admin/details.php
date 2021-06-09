609b4fd052a752db9f7de8e2960053513429680b
rerudharma <55394906+rerudharma@users.noreply.github.com>
2021-06-09 10:59:35 -0500

    Update Desain HTML CSS

--- admin/details.php
+++ admin/details.php
@@ -47,40 +47,100 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Details | Hellopet</title>
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+    <link rel="icon" href="logo.png" type="image/x-icon">
 </head>
+<style>
+body{
+    background: url('gropack.png');
+}
+#pack{
+    position: absolute;
+    width: 600px;
+    height: 630px;
+    left: 390px;
+    top: 10px;
+    border-radius: 12px;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 20px rgba(121, 98, 77, 0.5);
+    background: url('dog.png');
+ }
+ .title {
+    font-family: Raboto;
+    text-align: center;
+}
+.text{
+    margin-left: 20px;
+}
+.back {
+  background-color: #0084B4;
+  color: white;
+  padding: 12px;
+  margin: 0;
+  border: none;
+  width: 15%;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 17px;
+}
+.back:hover {
+  background-color: #90000d;
+}
+.set {
+  background-color: #0084B4;
+  color: white;
+  padding: 12px;
+  margin: 0;
+  margin-left: -18px;
+  border: none;
+  width: 30%;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 17px;
+}
+.set:hover {
+  background-color: #45a049;
+}
+</style>
 <body>
-    
-    <button onclick="window.history.back();">Kembali</button>
-    <h1>Order Details</h1>
+    <div id="pack">
+    <button class="back" onclick="window.history.back();">Kembali</button>
+    <header class="title">
+        <h1>Order Details</h1>
+    </header>
+    <div class="text">
+        <h3>Pet Details</h3>
+        <ul>
+            <li>Pet Type : <?php if( $pet=='anjing' ) echo 'Dog'; else echo 'Kucing';?></li>
+            <li>Pet Size : <?php if( $size=='kecil' ) echo 'Small'; else if( $size=='sedang' ) echo 'Medium'; else echo 'Besar';?></li>
+            <li>Pet Age : <?=$age?> month</li>
+            <li>Pet Gender : <?php if( $gender=='jantan' ) echo 'Male'; else echo 'Female'?></li>
+        </ul>
 
-    <h3>Pet Details</h3>
-    <ul>
-        <li>Pet Type : <?php if( $pet=='anjing' ) echo 'Dog'; else echo 'Kucing';?></li>
-        <li>Pet Size : <?php if( $size=='kecil' ) echo 'Small'; else if( $size=='sedang' ) echo 'Medium'; else echo 'Besar';?></li>
-        <li>Pet Age : <?=$age?> month</li>
-        <li>Pet Gender : <?php if( $gender=='jantan' ) echo 'Male'; else echo 'Female'?></li>
-    </ul>
-    <h3>Customer Details</h3>
-    <ul>
-        <li>Name : <?=$nama?></li>
-        <li>Email : <?=$email?></li>
-        <li>Phone : <?=$phone?></li>
-        <li>Adress : <?=$address?></li>
-        <li>Appointment : <?=$date,", ", $time?></li>
-    </ul>
-    <h3>Package Details</h3>
-    <ul>
-        <li>Package : <?=$pname?></li>
-        <li>Cost : <?=$price?></li>
-    </ul>
-    <h3>Payment Details</h3>
-    <ul>
-        <li>Method : <?php if( $payment=='transfer' ) echo 'Transfer'; else echo 'Pay After Service';?></li>
-        <li>Status : <?php if( $status_bayar=='sudah bayar' ) echo 'Paid'; else echo 'Not yet paid';?></li>
-        <?php if( $status_bayar!='sudah bayar' ) : ?>
-            <li><button onclick="window.location.href='paid.php?id=<?=$id?>'">Set to Paid</button></li>
-        <?php endif;?>
-    </ul>
+        <h3>Customer Details</h3>
+        <ul>
+            <li>Name : <?=$nama?></li>
+            <li>Email : <?=$email?></li>
+            <li>Phone : <?=$phone?></li>
+            <li>Adress : <?=$address?></li>
+            <li>Appointment : <?=$date,", ", $time?></li>
+        </ul>
+
+        <h3>Package Details</h3>
+        <ul>
+            <li>Package : <?=$pname?></li>
+            <li>Cost : <?=$price?></li>
+        </ul>
+
+        <h3>Payment Details</h3>
+        <ul>
+            <li>Method : <?php if( $payment=='transfer' ) echo 'Transfer'; else echo 'Pay After Service';?></li>
+            <li>Status : <?php if( $status_bayar=='sudah bayar' ) echo 'Paid'; else echo 'Not yet paid';?></li>
+            <?php if( $status_bayar!='sudah bayar' ) : ?>
+                <li><button class="set" onclick="window.location.href='paid.php?id=<?=$id?>'">Set to Paid</button></li>
+            <?php endif;?>
+        </ul>
+    </div>
     
 </body>
 </html>
