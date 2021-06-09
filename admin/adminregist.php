0303f74185c4ddc14718a78a32553d72b6e0921f
rerudharma <55394906+rerudharma@users.noreply.github.com>
2021-06-09 10:50:42 -0500

    Update adminregist.php
    update desain html css

--- admin/adminregist.php
+++ admin/adminregist.php
@@ -13,40 +13,102 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<title>Halaman Registrasi</title>
-</head>
+	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+	<link rel="icon" href="logo.png" type="image/x-icon">
+<style>
+	body{
+	background: url('gropack.png');
+}
+#pack{
+ 	position: absolute;
+	width: 470px;
+	height: 320px;
+	left: 455px;
+	top: 160px;
+	border-radius: 12px;
+	background: #FFFFFF;
+	box-shadow: 0px 4px 28px rgba(121, 98, 77, 0.5);
+	background: url('dog.png');
+ }
+.title {
+	font-family: Raboto;
+	text-align: center;
+}
+.row {
+  display: -ms-flexbox; /* IE10 */
+  display: flex;
+  -ms-flex-wrap: wrap; /* IE10 */
+  flex-wrap: wrap;
+  padding-right: 10px;
+  padding-top: 20px;
+  padding-left: 30px;
+  margin: 0 -16px;
+}
+
+.registrasi{
+	position: absolute;
+	width: 78px;
+    height: 35px;
+	padding-right: 10px;
+	margin-top: 28px;
+	margin-left: 278px;
+	color: #ffffff;
+    background: #9edffd;
+    border-radius: 5px;
+    border: none;
+	background: -webkit-linear-gradient(bottom, #bebebe 1%, #7f8cff 10%);
+    background: -moz-linear-gradient(bottom, #bebebe 1%, #7f8cff 10%);
+    background: linear-gradient(bottom, #bebebe 1%, #7f8cff 10%);
+    cursor: pointer;
+    font-family: century Gothic;
+    font-style: bold;
+}
+
+</style>
 <body>
-
-	<h1>Halaman Registrasi</h1>
-
+	<div class="background">
+	<div id="pack">
+		<header class="title">
+			<h1>Halaman Registrasi</h1>
+		</header>	
 	<form action="adminregistrasi.php" method="post">
 		
 		<ul>
-			
-			<li>
-				
-				<label for="username">Username : </label>
-				<input type="text" name="username" id="username">
+			<div class="row">
+				<li>
+					<label for="username">Username &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp : </label>
+					<input type="text" name="username" id="username">
+				</li>
+			</div>
+			<div class="row">
+				<li>
+					<label for="password">Password &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp: </label>
+					<input type="password" name="password" id="password">
 
-			</li>
-			<li>
-				
-				<label for="password">Password : </label>
-				<input type="password" name="password" id="password">
+				</li>
+			</div>
+			<div class="row">
+				<li>
+					<label for="konfpassword">Konfirmasi Password &nbsp: </label>
+					<input type="password" name="konfpassword" id="konfpassword">
+				</li>
+			</div>
 
-			</li>
-			<li>
-				
-				<label for="konfpassword">Konfirmasi Password : </label>
-				<input type="password" name="konfpassword" id="konfpassword">
-
-			</li>
-
-			<button type="submit" name="register">Register!</button>
+			<button class="registrasi" type="submit" name="register">Register!</button>
 
 		</ul>
 
 	</form>
-
+	</div>
+	</div>
 </body>
 </html>
