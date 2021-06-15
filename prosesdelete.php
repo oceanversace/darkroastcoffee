0a96b0b02ad224568f921f5b8e0f9adc8565d9b0
fullvelocity1 <herdika.s1@gmail.com>
2021-06-15 12:50:31 +0700

    fixed delete

--- prosesdelete.php
+++ prosesdelete.php
@@ -3,6 +3,7 @@
 	session_start();
 
 	$usr_id = $_SESSION['id'];
+	$query = mysqli_query($conn, "DELETE FROM feedback WHERE cust_id=$usr_id");
 	$query = mysqli_query($conn, "DELETE FROM appointment WHERE user_id=$usr_id");
 	$query = mysqli_query($conn, "DELETE FROM `user` WHERE `user`.`id` = $usr_id");
 	if ($query) {
